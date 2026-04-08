da453b29a63f426296cfc18a8e7191c311a2095d
shakuntla Devi <sd2511825@gamil.com>
2026-04-08 20:49:12 +0530

    Updated CORS and deploy fixes

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -9,9 +9,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express()
-app.use(cors({
-  origin: "http://localhost:5173"
-}));
+app.use(cors());
 app.use(express.json())
 
 app.use("/api/users", userRoutes)
